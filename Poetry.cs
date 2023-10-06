dea879c50d559ee0fcd6546e71882a8fc8425066
guille <guillenazua@gmail.com>
2023-10-06 22:09:52 +0200

    adding Poetry.cs class so that I can use its properties

---
+++ Poetry.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apiConnection
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Poetry>>(myJsonResponse);
+    public class Poetry
+    {
+        [JsonProperty("lines")]
+        public List<string> Lines;
+    }
+
+
+}
